d3d22bab8b3d9d73e46aa2b9e4d1ff93d949a29a
Allan W <51011089+ertyertyerty@users.noreply.github.com>
2024-01-29 22:08:16 +0000

    Finishing off chapter 1

--- Start/Ch 1/class_start.py
+++ Start/Ch 1/class_start.py
@@ -4,26 +4,50 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
 
     # TODO: double-underscore properties are hidden from other classes
+    __booklist = None
 
     # TODO: create a class method
+    @classmethod
+    def get_book_types(cls):
+        return cls.BOOK_TYPES
 
     # TODO: create a static method
+    def getbooklist():
+        if (Book.__booklist == None):
+            Book.__booklist = []
+        return Book.__booklist
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def set_title(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} not a valid book type")
+        else:
+            self.booktype = booktype
 
 
 # TODO: access the class attribute
-
+print("Book types:", Book.get_book_types())
 
 # TODO: Create some book instances
+b1 = Book("Title1", "HARDCOVER")
+b2 = Book("Title2", "PAPERBACK")
+b3 = Book("Title3", "EBOOK")
 
 
 # TODO: Use the static method to access a singleton object
+thebooks = Book.getbooklist()
+thebooks.append(b1)
+thebooks.append(b2)
+print("First round: ", thebooks)
+morebooks = Book.getbooklist()
+print("Second round: ", morebooks)
+morebooks.append(b3)
+print("Third round:", thebooks)
